eb249f20dc1a4b898d39e14a99326685bcf2bf9a
abdulrahman <AbdulrahmanHossam@email.com>
2024-04-25 19:36:23 +0200

    Adding the Create Token Logic

--- ChatApp.API/Services/TokenService.cs
+++ ChatApp.API/Services/TokenService.cs
@@ -1,12 +1,47 @@
 ﻿using ChatApp.API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ChatApp.API.Services
 {
     public class TokenService : ITokenService
     {
+        //private readonly IConfiguration _configuration;
+
+        // create key and configruation to this key
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration configuration)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
+        }
+
         public string CreateToken(AppUser user)
         {
-            
+            // create claims by adding claim type & value
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            // create credential and this will be the token header this take key and algorithm
+            var credential = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            // add componant to tokenDescriptor to generate token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = credential
+            };
+
+            // use token handler to handle token create and write
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
