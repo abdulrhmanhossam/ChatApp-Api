a2f76764a2ec3958d5286632d0c18b1f51d661dd
abdulrahman <abdulrahmn398@outlook.com>
2024-07-17 13:09:49 +0300

    creating a message hub

---
+++ ChatApp.API/SignalR/MessageHub.cs
@@ -0,0 +1,38 @@
+﻿using ChatApp.API.Extensions;
+using ChatApp.API.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.API.SignalR;
+
+public class MessageHub(IMessageRepository messageRepository) : Hub
+{
+    public override async Task OnConnectedAsync()
+    {
+        var httpContext = Context.GetHttpContext();
+        var otherUser = httpContext.Request.Query["user"];
+
+        if (Context.User == null || string.IsNullOrEmpty(otherUser))
+            throw new Exception("Cannot join group");
+
+        var groupName = GetGroupName(Context.User.GetUserName(), otherUser);
+
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+        var messages = await messageRepository
+            .GetMessageThread(Context.User.GetUserName(), otherUser);
+
+        await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+    }
+
+    public override Task OnDisconnectedAsync(Exception exception)
+    {
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    private string GetGroupName(string caller, string other)
+    {
+        var stringCompare = string.CompareOrdinal(caller, other) < 0;
+
+        return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+    }
+}
